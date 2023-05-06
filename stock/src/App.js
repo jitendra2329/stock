9a012581b469eb8960a90e63241079b9272e6b4a
Jitendra <jitendra.kumar@knoldus.com>
2023-05-06 12:44:19 +0530

    table

--- stock/src/App.js
+++ stock/src/App.js
@@ -11,6 +11,18 @@ function Form({ onSubmit }) {
   //   onSubmit({ symbol, socialMedia, timeWindow });
   // };
 
+
+const data =  [
+  { "date": "2023-05-01", "price": 134.08, "socialMediaCount": 1200 },
+  { "date": "2023-05-02", "price": 137.05, "socialMediaCount": 1100 },
+  { "date": "2023-05-03", "price": 139.32, "socialMediaCount": 950 },
+  { "date": "2023-05-04", "price": 141.63, "socialMediaCount": 800 },
+  { "date": "2023-05-05", "price": 144.12, "socialMediaCount": 900 },
+  { "date": "2023-05-06", "price": 147.16, "socialMediaCount": 1000 },
+  { "date": "2023-05-07", "price": 149.12, "socialMediaCount": 800 },
+  { "date": "2023-05-09", "price": 151.94, "socialMediaCount": 600 }
+ ]
+
   return (
     <center>
     <div  className="container">
@@ -35,7 +47,29 @@ function Form({ onSubmit }) {
       <button type="submit" >Submit</button>
     </form>
     </div>
+
+    <table border={1}>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Price</th>
+              <th>S.M.count</th>
+              <th>Reccomendation</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => (
+              <tr key={index}>
+                <td>{item.date}</td>
+                <td>{item.price}</td>
+                <td>{item.socialMediaCount}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
     </center>
+
+    
   );
 }
 
--- stock/src/index.js
+++ stock/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
 import Form from './App';
+// import StockData from './StockData';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Form />
+    {/* <StockData/> */}
   </React.StrictMode>
 );
 
